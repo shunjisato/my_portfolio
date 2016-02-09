b8f6e1070e1bf486c5b81b0870fc18c70f1732fb
ShunjiSato <shunjisato222@gmail.com>
2016-02-09 17:21:07 +0900

    add layout.html

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", title="Home")
+
+@app.route("/about")
+def about():
+	return render_template("about.html", title="About")
+
+@app.route("/works")
+def works():
+	return render_template("works.html", title="Works")
 
 if __name__ == "__main__":
 	app.run()
